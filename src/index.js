0fdc1c1a0752517112f12bb5c650275cc5a2be68
KDeepak5673 <deepakchandrashekhar0309@gmail.com>
2024-03-16 17:16:28 +0530

    connect database

---
+++ src/index.js
@@ -0,0 +1,52 @@
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+// import mongoose from "mongoose";
+// import {DB_NAME} from "./constants";
+
+import { Express } from "express";
+const app = express()
+
+(async () => {
+    try {
+        mongoose.connect(`${process.env.MONGODB_URI}/$
+        {DB_NAME}`)
+        app.on('errror',(error) => {
+            console.log("ERRR: " , error);
+            throw error
+        })
+
+        app.listen(process.env.PORT , () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+
+    } catch (error) {
+        console.error("ERROR" , error)
+        throw error
+    }
+})()
+*/
